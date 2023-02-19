0882c5b62b9645e25f367f8eae7282cc8152b476
Olorode Adebanke <108427424+Jenrade112@users.noreply.github.com>
2023-02-19 19:45:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+//Question 1
+//Arithmetic Operator (+ , - , * , / )
+//Assignment Operator( = , += , -=, *=, /=)
+//Logical operator ( -&& , OR , -||)
+//Comparison operator ( > , < , == , <= , != , |==, ===)
+//Bitwise operator( & , | , ^ , <<)
+
+
+
+//Question 2
+//Arithmetic
+let x= 6;
+let y= 10;
+console.log (x+y)
+
+let d=12;
+let s= 2;
+console.log(d*s)
+//Assignment
+let u= 10;
+x -=5;
+console.log(u-5)
+
+let v= 2;
+v *= 5;
+console.log(v * 5 )
+
+//Comparison
+let z= 43;
+console.log(z>10);
+
+let b= -20;
+console.log (b>50)
+
+//Logical
+let banke=10;
+let soji=20;
+let dola=30;
+console.log (banke<soji && soji < dola )
+
+let dupeAge=55;
+let dapoAge=30;
+let ZealAge= 20;
+console.log ( dupeAge > dapoAge || dapoAge > ZealAge );
+
+//Bitwise
+let a =5;
+let c= 3;
+let t= a & c;
+console.log (t);
+
+let funke=5;
+let sope=3;
+let titi =funke | sope ;
+ console.log(titi)
+
+//Question 3
+let classGroup=(" Arts");
+if(classGroup==="Science")
+   console.log( "physics, Chemistry, biology, Technical Drawing, English ,and Mathmatics" );
+  else if (classGroup=== "social Sciences") 
+    console.log ("Accounting,Commerce, Marketing, Geography , English, and Mathematics");
+
+ else if ( classGroup=== "Arts Subject ") {
+    console.log("Government, Economics, Literature, Histoy,English, and Mathematics");
+ }
+ else 
+ console.log("English and Mathematics");
+
+ //Question 4
+ for (let i=1; i < 20; i += 7){
+ console.log(i);}
+
+//Question 5
+num=(40);
+let pwr=(50);
+console.log (2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
